a1ed8614f6e5392aa8d04ac1e7c2c089d77bd33c
VitaliiBobylov <bobylevvitalyanatolievich@gmail.com>
2025-08-10 14:54:20 +0300

    non

--- src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: 'https://furniture-store.b.goit.study/api',
-  timeout: 5000,
-});
--- src/main.js
+++
@@ -1,4 +0,0 @@
-import './furniture-list';
-
-import { api } from './api.js';
-import { initFeedback } from './js/feedback.js';
